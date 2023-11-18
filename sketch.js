e74261d56631282e9235bf48c4a3b8f98b1aa607
Circulator <moruge@hotmail.com>
2023-11-18 13:51:30 +0900

    width

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const modules = [];
 const moduleWidth = 100;
 const moduleHeight = 100;
 
-const startsquare_width = 1800;
+const startsquare_width = 200;
 const startsquare_height = 100;
 
 let time = 0;
@@ -17,7 +17,7 @@ class ColorBox {
 }
 
 function setup() {
-  createCanvas(800, 900);
+  createCanvas(1800, 900);
   colorMode(HSB, 150, 100, 100);
   //background('#333333');
 
@@ -26,8 +26,8 @@ function setup() {
       modules.push(new Module(generateCells(), startsquare_width + 200 * j, startsquare_height + 200 * i, moduleWidth, moduleHeight, moduleColumns, moduleRows, i));
     }
   }
-  
-  for (let i = 1; i < 5; i++){
+
+  for (let i = 1; i < 5; i++) {
     waves.push(new Wave(i));
   }
 }
@@ -45,81 +45,80 @@ function generateCells() {
 
 function draw() {
   background('#333333');
-  for (let i = 0; i < 4; i++){
+  for (let i = 0; i < 4; i++) {
     waves[i].updateWave();
   }
-  
-  for (const m of modules) {
-    m.drawModule();
-  }
 
-  // Stroke
+  // Draw vertical lines
   for (let i = startsquare_width + 50; i < startsquare_width + 1000; i += 200) {
     line(i, startsquare_height - 50, i, startsquare_height + 200 * 3 + 150);
     stroke(70);
   }
-  
+
+  // Calculate the number of squares and the spacing
+  const squareCols = 5;
+  const squareRows = 4;
+  const totalSpacingX = 200 * 5; // Total horizontal spacing
+  const totalSpacingY = 200 * 3; // Total vertical spacing
+
+  // Calculate spacing for squares to fit in the middle of each row and column
+  const spacingX = (width - startsquare_width - totalSpacingX - squareCols * moduleWidth) / (squareCols - 1);
+  const spacingY = (height - startsquare_height - totalSpacingY - squareRows * moduleHeight) / (squareRows - 1);
+
+  // Draw the grid of squares (5x4) aligned with vertical lines
+  for (let i = 0; i < squareCols; i++) {
+    for (let j = 0; j < squareRows; j++) {
+      const squareX = startsquare_width + i * (moduleWidth + spacingX+75);
+      const squareY = startsquare_height + j * (moduleHeight + spacingY+166.5);
+      fill(random(0, 20), random(0, 20), random(0, 80));
+      rect(squareX, squareY, moduleWidth, moduleHeight);
+    }
+  }
+
+  // Draw the modules on top
+  for (const m of modules) {
+    m.drawModule();
+  }
+
   time += 0.05;
-
-
 }
 
-class Wave{
-  constructor(yPos){
+class Wave {
+  constructor(yPos) {
     this.yPos = yPos;
     this.time = 0;
     this.data = [];
   }
-  
+
   updateWave() {
-  push();
-  translate(200, 200*this.yPos-50);
+    push();
+    translate(100, 200 * this.yPos - 50);
 
-  let radius = 30;
-    
+    let radius = 30;
 
-  let x = radius * cos(this.time);
-  let y = radius * sin(this.time);
+    let x = radius * cos(this.time);
+    let y = radius * sin(this.time);
 
-  this.data.unshift(y);
+    this.data.unshift(y);
 
-  fill(40);
-  beginShape(); // Start drawing a continuous line
-  for (let i = 0; i < this.data.length; i++) {
-    vertex(i, this.data[i]);
+    // Set the stroke color to blue
+    stroke(0, 0, 255);
+    fill(30);
+    strokeWeight(0.5);
+    beginShape();
+    for (let i = 0; i < this.data.length; i++) {
+      vertex(i, this.data[i]);
+    }
+    endShape();
+
+    // Reset the stroke color to its original value
+    stroke(0);
+
+    pop();
+    this.time += 0.05;
   }
-  endShape(); // End drawing the continuous line
-      
-  pop();
-  this.time += 0.05;
-  //this.time += 0;
-  //this.time += 0;
-}
-  
-  
 }
 
-
-function updateWave(yPos) {
-  push();
-  translate(200, 150*yPos);
-
-  let radius = 50;
-
-  let x = radius * cos(time);
-  let y = radius * sin(time);
-
-  wave.unshift(y);
-
-  noFill();
-  beginShape(); // Start drawing a continuous line
-  for (let i = 0; i < wave.length; i++) {
-    vertex(i, wave[i]);
-  }
-  endShape(); // End drawing the continuous line
-  pop();
-  time += 0.05;
-}
 
 class Module {
   constructor(cells, coordX, coordY, moduleWidth, moduleHeight, columns, rows, rowOffset) {
@@ -127,7 +126,6 @@ class Module {
     this.columns = columns;
     this.rows = rows;
     this.moduleWidth = moduleWidth;
-    //this.moduleWidth = moduleWidth*2;
     this.moduleHeight = moduleHeight;
     this.coordX = coordX;
     this.coordY = coordY;
@@ -141,21 +139,22 @@ class Module {
     push();
     translate(this.coordX, this.coordY);
 
-    fill(random(0, 200), random(0, 20), random(50, 100));
-
-    rect(0, 0, this.moduleWidth, this.moduleHeight);
-
-    for (let r = 0 ; r < this.rows; r++) {
+    for (let r = 0; r < this.rows; r++) {
       for (let c = 0; c < this.columns / 2; c++) {
         this.cells[r][c] = constrain(this.cells[r][c], 0, random(0, 130));
 
         const y = this.moduleHeight * (r / this.rows);
         const x = this.moduleWidth * (c / this.columns);
 
-        // Apply wave effect to the cells with row-specific offset
         const yOffset = waveEffect(x, y, this.rowOffset);
-        fill(this.cells[r][c], random(0, 10), random(0, 50));
-        rect(x, y + yOffset, cellWidth, cellHeight);
+
+        fill(random(0, 40), random(0, 40), random(0, 80));
+        beginShape();
+        vertex(x, y + yOffset);
+        vertex(x + cellWidth, y + yOffset);
+        vertex(x + cellWidth, y + yOffset + cellHeight);
+        vertex(x, y + yOffset + cellHeight);
+        endShape(CLOSE);
       }
     }
     pop();
@@ -163,17 +162,13 @@ class Module {
 }
 
 function waveEffect(x, y, rowOffset) {
-  // Adjust this value to control the magnitude of the wave effect
-  //const magnitude = 20;
-  const magnitude =50;
-  
+  const magnitude = 0;
+
   return magnitude * sin(time * 1 + x / 50 + y / 50 + rowOffset * 0.5);
-  //return magnitude * sin(time * 2 + x / 50 + y / 50 + rowOffset * 0.5);
 }
 
 function keyPressed() {
   if (key === 'e' || key === 'E') {
-    // Save the canvas as 'myCanvas.png'
     saveCanvas('myCanvas', 'png');
   }
 }
